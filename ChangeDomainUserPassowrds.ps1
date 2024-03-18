ce6d6ddb15c214e7532cc9920cc8ed3b6a4e934e
lli555 <90268394+lli555@users.noreply.github.com>
2024-03-18 14:27:11 -0400

    Create ChangeDomainUserPassowrds.ps1

---
+++ ChangeDomainUserPassowrds.ps1
@@ -0,0 +1,33 @@
+# Script to change all domain user passwords, excluding Admin
+
+Import-Module ActiveDirectory
+
+# Function to generate a random password
+Function Get-RandomPassword {
+    # Define parameter with password length
+    param([int]$PasswordLength = 20)
+
+    # Store combinations in a set
+    $CharacterSet = @{
+            Lowercase   = (97..122) | Get-Random -Count 10 | % {[char]$_}
+            Uppercase   = (65..90)  | Get-Random -Count 10 | % {[char]$_}
+            Numeric     = (48..57)  | Get-Random -Count 10 | % {[char]$_}
+            SpecialChar = (33..47)+(58..64)+(91..96)+(123..126) | Get-Random -Count 10 | % {[char]$_}
+    }
+
+    # Frame random password
+    $StringSet = $CharacterSet.Lowercase + $CharacterSet.Uppercase + $CharacterSet.Numeric + $CharacterSet.SpecialChar
+    -join(Get-Random -Count $PasswordLength -InputObject $StringSet)
+  
+
+}
+
+
+$users = Get-ADUser -Filter {Name -notlike "Admin"} -Properties * -SearchBase "OU=umasscybersec, DC=umasscybersec,DC=com"
+foreach ($user in $users) {
+   $username = $user.Name
+   $plainPassword = Get-RandomPassword
+   $newPassword = ConvertTo-SecureString $plainPassword -AsPlainText -Force
+   Write-Output("Username: $username, New Password: $plainPassword")
+   Set-ADAccountPassword -Identity $username -Reset -NewPassword $newPassword
+} 
