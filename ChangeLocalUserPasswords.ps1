d8eb95f55107529c6a03a3ade36f4ce13b23d17e
lli555 <90268394+lli555@users.noreply.github.com>
2024-03-18 15:50:08 -0400

    ChangeLocalUserPasswords
    Backup script, needs testing

---
+++ ChangeLocalUserPasswords.ps1
@@ -0,0 +1,49 @@
+# Script to change local account passwords on each computer
+Import-Module ActiveDirectory
+
+Function Get-RandomPassword {
+    # Define parameter with password length
+    param([int]$PasswordLength = 20)
+
+    # Store password combinations into a set
+    $CharacterSet = @{
+        Lowercase = (97..122) | Get-Random -Count 10 | % {[char]$_}
+        Uppercase = (65..90) | Get-Random -Count 10 | % {[char]$_}
+        Numeric = (48..57) | Get-Random -Count 10 | %{[char]$_}
+        SpecialChar = (33..47)+(58..64)+(91..96)+(123..126) | Get-Random -Count 10 | % {[char]$_}
+    }
+
+    # Frame random password
+    $StringSet = $CharacterSet.Lowercase + $CharacterSet.Uppercase + $CharacterSet.Numeric + $CharacterSet.SpecialChar
+    -join(Get-Random -Count $PasswordLength -InputObject $StringSet)
+}
+
+
+# Find and store all ad computers
+$computers = Get-ADComputer -Filter { OperatingSystem -notlike "*Server*"}
+foreach ($each in $computers) {
+    $computer = $each.Name
+
+    # Test connection status
+    if (test-Connection -Cn $computer -quiet) {
+        try {
+            # get all local users on the computer
+            $users = Invoke-Command -ComputerName $computer -ScriptBlock {Get-LocalUser -name "username" | select Username}
+            
+            # change password for each user
+            foreach($user in $users) {
+                $plainPassword = Get-RandomPassword
+                $newPassword = ConvertTo-SecureString $plainPassword -AsPlainText -Force
+                Set-LocalUser -Password $newPassword
+                Write-Output("Password change successful")
+            }
+        }
+        catch {
+            Write-Output("Password change failed")
+        }
+
+    }
+    else {
+        Write-Output("$computer is offline")
+    }
+}
